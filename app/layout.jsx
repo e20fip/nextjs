282cba682b4951e79cedf92d423648717dade329
e20fip <nothingbest@gmail.com>
2023-04-15 11:08:31 +0700

    v1

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,11 +1,22 @@
+import { Sarabun } from 'next/font/google'
 import Header from './header'
 import Footer from './footer'
 import './globals.css'
 
+export const metadata = {
+  title: 'myapp',
+  description: 'learn nextjs'
+}
+
+const sarabun = Sarabun({
+  subsets: ['thai'],
+  weight: ['400', '600']
+})
+
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body>
+    <html lang="th">
+      <body style={sarabun.style}>
         <main>
           <Header />
           <div className="container">{children}</div>
--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,22 @@
-async function getData() {
-  const res = await fetch(
-    'https://jsonplaceholder.typicode.com/posts?_limit=10'
-  )
-  if (!res.ok) {
-    throw new Error('Failed to fetch data')
-  }
+import clientPromise from '@/lib/mongodb'
+import styles from './page.module.css'
+import Image from 'next/image'
 
-  return res.json()
+async function getData() {
+  try {
+    const client = await clientPromise
+    const db = await client.db('mongoTest')
+
+    const notes = await db
+      .collection('notes')
+      .find({})
+      .sort({ createdAt: -1 })
+      .toArray()
+
+    return notes
+  } catch (e) {
+    console.log(e)
+  }
 }
 
 export default async function Page() {
@@ -14,12 +24,33 @@ export default async function Page() {
 
   return (
     <>
-      {datas?.map((data) => (
-        <ul key={data.id}>
-          <li>{data.title}</li>
-          <li>{data.body}</li>
-        </ul>
-      ))}
+      <HeaderImage />
+      <div className={styles.content}>
+        {datas?.map((data) => (
+          <ul key={data._id}>
+            <li>
+              <span className={styles.title}>{data.title}</span>
+            </li>
+            <li>
+              <div dangerouslySetInnerHTML={{ __html: data.content }} />
+            </li>
+          </ul>
+        ))}
+      </div>
     </>
+  )
+}
+
+function HeaderImage() {
+  return (
+    <div className={styles.headimages}>
+      <Image
+        priority
+        fill
+        src="/images/web-pillow.jpg"
+        alt="pillow"
+        style={{ objectFit: 'cover' }}
+      />
+    </div>
   )
 }
