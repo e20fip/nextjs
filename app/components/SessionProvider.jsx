0d1d30b560f3cbdfb7cd69d24bb92912b1c30272
e20fip <nothingbest@gmail.com>
2024-02-20 19:34:34 +0700

    testing vercel gateway timeout error

--- app/components/SessionProvider.jsx
+++ app/components/SessionProvider.jsx
@@ -1,3 +1,5 @@
 "use client"
 import { SessionProvider } from "next-auth/react"
-export default SessionProvider
+export default function SessionProvider({ children }) {
+  return <SessionProvider>{children}</SessionProvider>
+}
